145cb073f9e85fba9659c46c87e1a514c52e27d8
fidashn <87206839+fidashn@users.noreply.github.com>
2022-11-04 20:34:00 +0200

    editted the font family for the h1

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <main className='bg-white px-10'>
         <section className='min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl'>Fida Shnakher|Frontend Developer</h1>
+            <h1 className='text-xl font-burtons'>Fida Shnakher|Frontend Developer</h1>
             <ul className='flex items-center'>
              <li><BsFillMoonStarsFill className='cursor-pointer text-2xl'/></li>
              <li><a className='bg-gradient-to-r from-purple-400 to-teal-300 text-white px-8 py-2 rounded-3xl text-lg ml-8 ' href="../public/Fida-Sh-CV-FrontEnd.pdf">Resume</a></li> 
